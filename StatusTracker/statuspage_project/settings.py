557d5d3189740feff667b07423294cef82e98d97
Katam Veera Kowshik <veerakowshikkatam@gmail.com>
2025-05-16 23:07:11 +0530

    added all requirements

--- StatusTracker/statuspage_project/settings.py
+++ StatusTracker/statuspage_project/settings.py
@@ -1,7 +1,7 @@
 """
 Django settings for statuspage_project project.
 """
-
+import dj_database_url
 import os
 from pathlib import Path
 
@@ -12,15 +12,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6vw9jc1=^&@b64f$_)#$@+^uu3o2h@yb8e=kf2=@%xwq+c)g7@'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', "False").lower() == "true"
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 # CSRF settings
-CSRF_TRUSTED_ORIGINS = ['https://*.repl.co', 'https://*.replit.app']
+# CSRF_TRUSTED_ORIGINS = ['https://*.repl.co', 'https://*.replit.app']
 
 # Application definition
 
@@ -82,27 +82,28 @@ ASGI_APPLICATION = 'statuspage_project.routing.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 # Check if DATABASE_URL environment variable exists
-if os.environ.get('DATABASE_URL'):
-    # Use PostgreSQL
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('PGDATABASE', ''),
-            'USER': os.environ.get('PGUSER', ''),
-            'PASSWORD': os.environ.get('PGPASSWORD', ''),
-            'HOST': os.environ.get('PGHOST', ''),
-            'PORT': os.environ.get('PGPORT', ''),
-        }
-    }
-else:
+# if os.environ.get('DATABASE_URL'):
+#     # Use PostgreSQL
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql',
+#             'NAME': os.environ.get('PGDATABASE', ''),
+#             'USER': os.environ.get('PGUSER', ''),
+#             'PASSWORD': os.environ.get('PGPASSWORD', ''),
+#             'HOST': os.environ.get('PGHOST', ''),
+#             'PORT': os.environ.get('PGPORT', ''),
+#         }
+#     }:
     # Use SQLite for local development
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES = {
+    'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+            }
         }
-    }
 
+database_url = os.environ.get("DATABASE_URL")
+DATABASES["default"] = dj_database_url.parse(database_url)
 # Channel layers for websocket support
 CHANNEL_LAYERS = {
     'default': {
